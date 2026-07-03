beb04107ece960048b79d5befbbfc0b6b25aeb09
KAVIN KUMAR <kavinkumarkumar184@gmail.com>
2026-07-03 17:47:23 +0100

    Create 0011_Container_with_Most_water.py
    Container with Most Water is easiest and logical thinking program which I have done with while loop as two pointer of left and right phenomenon.
    Container with Most Water is easiest and logical thinking program which I have done with while loop as two pointer of left and right phenomenon
    Time complexity:
    O(n) -->each pointer moves at most n times
    
    Space complexity:
    O(1) -->only few variables are used

---
+++ 0011_Container_with_Most_water.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maxArea(self, height):
+        l=0
+        r=len(height)-1
+        res=0
+        while l<r:
+            d=r-l
+            w=min(height[r],height[l])
+            a=d*w
+            if res<a:
+                res=a
+            elif height[l]<height[r]:
+                l=l+1
+            else:
+                r=r-1
+        return res
+
+
+#time complexity -->O(n)
+#space complexity -->O(1)
